e3e26246560fffc05624acb4298a47848b0e7144
bthisstriving <bthanhtrx@gmail.com>
2021-09-25 17:17:08 +0700

    Initial Commit

---
+++ app/src/main/java/com/nerothtr/weatherapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.nerothtr.weatherapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.savvynomad.weatherapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
